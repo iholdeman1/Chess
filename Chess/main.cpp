9a5cb8ce7233254c8779b21ab3ee81012e6a047c
iholdeman1 <ian.holdeman0510@gmail.com>
2020-08-01 12:28:32 -0400

    Initial Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by Ian Holdeman on 8/1/20.
+//  Copyright © 2020 Marvin LLC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
